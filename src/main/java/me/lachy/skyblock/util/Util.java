5f823e71d3359e44e61318c6eac6e26e83c40ae9
lachlantheslayer <lachlantheslayer@gmail.com>
2021-03-03 21:40:11 +0000

    Create Util class

---
+++ src/main/java/me/lachy/skyblock/util/Util.java
@@ -0,0 +1,17 @@
+package me.lachy.skyblock.util;
+
+public class Util {
+
+    public static String getMainPrefix() {
+        return "§2[SKYBLOCK]&r";
+    }
+
+    public static String getDebugPrefix() {
+        return "§9[DEBUG]&r";
+    }
+
+    public static String getDevPrefix() {
+        return "§c[DEV]&r";
+    }
+
+}
