36fd717db66ffb922741a18a11874cbc161c8ead
lachlantheslayer <lachlantheslayer@gmail.com>
2021-03-04 19:16:12 +0000

    Add feedback to item GUI

--- src/main/java/me/lachy/skyblock/commands/dev/ItemCommand.java
+++ src/main/java/me/lachy/skyblock/commands/dev/ItemCommand.java
@@ -6,6 +6,7 @@ import me.mattstudios.mfgui.gui.components.util.ItemBuilder;
 import me.mattstudios.mfgui.gui.guis.GuiItem;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -51,6 +52,8 @@ public class ItemCommand implements CommandExecutor {
                 plugin.items.sort(Comparator.comparing(o -> ChatColor.stripColor(o.getItemMeta().getDisplayName())));
                 plugin.items.forEach(itemStack -> gui.addItem(new GuiItem(itemStack, event -> {
                     event.getWhoClicked().getInventory().addItem(itemStack);
+                    event.getWhoClicked().getWorld().playSound(event.getWhoClicked().getLocation(), Sound.NOTE_PLING, 1.0F, 1.0F);
+                    event.getWhoClicked().sendMessage(Skyblock.debugPrefix + "§fYou were given " + itemStack.getItemMeta().getDisplayName());
                     event.setCancelled(true);
                     event.getWhoClicked().closeInventory();
                 })));
