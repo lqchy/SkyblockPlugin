835271cc653c561721b4e726e2f80bc4ab73663e
lachlantheslayer <lachlantheslayer@gmail.com>
2021-03-04 19:24:45 +0000

    Add event for both current items

--- src/main/java/me/lachy/skyblock/listeners/RightClickListener.java
+++ src/main/java/me/lachy/skyblock/listeners/RightClickListener.java
@@ -1,10 +1,14 @@
 package me.lachy.skyblock.listeners;
 
 import me.lachy.skyblock.Skyblock;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class RightClickListener implements Listener {
 
@@ -17,5 +21,19 @@ public class RightClickListener implements Listener {
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInHand();
+
+        if (!item.hasItemMeta()) return;
+        if (!player.getGameMode().equals(GameMode.SURVIVAL)) return;
+
+        switch (ChatColor.stripColor(item.getItemMeta().getDisplayName()).toLowerCase()) {
+            case "aspect of the jerry":
+                player.getWorld().playSound(player.getLocation(), Sound.VILLAGER_IDLE, 1.0F, 1.0F);
+                break;
+            case "grappling hook":
+                player.sendMessage("WIP");
+                break;
+        }
+
     }
 }
